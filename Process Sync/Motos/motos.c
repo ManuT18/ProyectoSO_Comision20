05707031a89edb45c5159070c567d6c43eca3de8
nicolastradivarius <gonzaleznicolas114@outlook.com>
2024-10-10 13:03:58 -0300

    Actualización Motos - Se agregó un mutex para asegurar (simular) acceso único a la sección crítica (imprimir la acción llevada a cabo). - Cambios en la sintaxis - En cada iteración, `process_ruedas` generará una sola rueda. Como efecto colateral, en N iteraciones, habrá N/2 ruedas, por ende, N/2 chasis, y así siguiendo. Esto provoca el bloqueo del programa dado que al llegar a la iteración N/2 + 1, `process_ruedas` no existe más, pero los otros sí, en particular `process_chasis` que se queda bloqueado en su semáforo porque no hay nadie que le haga signal.

--- Process Sync/Motos/motos.c
+++ Process Sync/Motos/motos.c
@@ -10,97 +10,113 @@
 #include <pthread.h>
 
 sem_t sem_ruedas, sem_chasis, sem_motor, sem_pintura, sem_extras;
+pthread_mutex_t mutex;
+
 pthread_t threads[6];
 
 //
-#define ITERATIONS 7
+#define ITERATIONS 10
 
-void *process_ruedas()
-{
-    for (int i = 0; i < ITERATIONS; i++)
-    {
+void *process_ruedas() {
+    for (int i = 0; i < ITERATIONS; i++) {
         sem_wait(&sem_ruedas);
+        pthread_mutex_lock(&mutex);
         printf("Rueda \n");
         fflush(stdout);
-        sem_post(&sem_chasis);
-
-        printf("Rueda \n");
-        fflush(stdout);
+        pthread_mutex_unlock(&mutex);
         sem_post(&sem_chasis);
     }
     return NULL;
 }
 
-void *process_chasis()
-{
-    for (int i = 0; i < ITERATIONS; i++)
-    {
+void *process_chasis() {
+    for (int i = 0; i < ITERATIONS; i++) {
         sem_wait(&sem_chasis);
         sem_wait(&sem_chasis);
+        pthread_mutex_lock(&mutex);
         printf("Chasis\n");
         fflush(stdout);
+        pthread_mutex_unlock(&mutex);
         sem_post(&sem_motor);
     }
     return NULL;
 }
 
-void *process_motor()
-{
-    for (int i = 0; i < ITERATIONS; i++)
-    {
+void *process_motor() {
+    for (int i = 0; i < ITERATIONS; i++) {
         sem_wait(&sem_motor);
+        pthread_mutex_lock(&mutex);
         printf("Motor\n");
         fflush(stdout);
+        pthread_mutex_unlock(&mutex);
         sem_post(&sem_pintura);
     }
     return NULL;
 }
 
-void *process_pinturaverde()
-{
-    for (int i = 0; i < ITERATIONS; i++)
-    {
+void *process_pinturaverde() {
+    for (int i = 0; i < ITERATIONS; i++) {
         sem_wait(&sem_pintura);
+        pthread_mutex_lock(&mutex);
         printf("Pintura verde\n--------------------\n");
         fflush(stdout);
-        sem_post(&sem_extras);
+
+        if (i % 2 == 0) {
+            sem_post(&sem_extras);
+        } else {
+            sem_post(&sem_ruedas);
+            sem_post(&sem_ruedas);
+        }
+        pthread_mutex_unlock(&mutex);
     }
     return NULL;
 }
 
-void *process_pinturaroja()
-{
-    for (int i = 0; i < ITERATIONS; i++)
-    {
+void *process_pinturaroja() {
+    for (int i = 0; i < ITERATIONS; i++) {
         sem_wait(&sem_pintura);
+        pthread_mutex_lock(&mutex);
         printf("Pintura roja\n--------------------\n");
         fflush(stdout);
-        sem_post(&sem_extras);
+
+        if (i % 2 == 0) {
+            sem_post(&sem_extras);
+        } else {
+            sem_post(&sem_ruedas);
+            sem_post(&sem_ruedas);
+        }
+        pthread_mutex_unlock(&mutex);
     }
     return NULL;
 }
 
-void *process_extras()
-{
-    for (int i = 0; i < ITERATIONS; i++)
-    {
+void *process_extras() {
+    // "Una de cada dos motos es elegida para recibir equipamiento extra".
+    // Esto se "logra" con el if (i % 2 == 0) (es la varialbe `i` par?) en los procedimientos de pintura.
+    // Sin embargo, la variable i es local a cada procedimiento, por lo que no se puede asegurar 
+    // un intercalado coherente: si la primer moto la pintan de rojo, y la segunda de verde, ambas
+    // recibirán equipamiento extra porque la variable i era par en ambos procedimientos.
+    // Por lo tanto, a los efectos del enunciado, se sigue cumpliendo, y hasta puede que quede mejor así.
+    for (int i = 0; i < ITERATIONS; i++) {
         sem_wait(&sem_extras);
-        sem_post(&sem_ruedas);
-        sem_wait(&sem_extras);
+        pthread_mutex_lock(&mutex);
         printf("Extras añadidos\n--------------------\n");
         fflush(stdout);
+        pthread_mutex_unlock(&mutex);
+        sem_post(&sem_ruedas);
         sem_post(&sem_ruedas);
     }
     return NULL;
 }
 
-int main(int argc, char const *argv[])
-{
-    sem_init(&sem_ruedas, 0, 1);
+int main(int argc, char const *argv[]) {
+    sem_init(&sem_ruedas, 0, 2);
     sem_init(&sem_chasis, 0, 0);
     sem_init(&sem_motor, 0, 0);
     sem_init(&sem_pintura, 0, 0);
     sem_init(&sem_extras, 0, 0);
+
+    pthread_mutex_init(&mutex, NULL);
 
     pthread_create(&threads[0], NULL, process_ruedas, NULL);
     pthread_create(&threads[1], NULL, process_chasis, NULL);
@@ -109,8 +125,7 @@ int main(int argc, char const *argv[])
     pthread_create(&threads[4], NULL, process_pinturaroja, NULL);
     pthread_create(&threads[5], NULL, process_extras, NULL);
 
-    for (int i = 0; i < 6; i++)
-    {
+    for (int i = 0; i < 6; i++) {
         pthread_join(threads[i], NULL);
     }
 
@@ -126,56 +141,3 @@ int main(int argc, char const *argv[])
 
     return 0;
 }
-
-/**
- * Se bloquea:
- * 
- * Rueda 
-Rueda 
-Chasis
-Motor
-Pintura verde
---------------------
-Rueda 
-Rueda 
-Chasis
-Motor
-Pintura verde
---------------------
-Extras añadidos
---------------------
-Rueda 
-Rueda 
-Chasis
-Motor
-Pintura roja
---------------------
-Rueda 
-Rueda 
-Chasis
-Motor
-Pintura verde
---------------------
-Extras añadidos
---------------------
-Rueda 
-Rueda 
-Chasis
-Motor
-Pintura roja
---------------------
-Rueda 
-Rueda 
-Chasis
-Motor
-Pintura verde
---------------------
-Extras añadidos
---------------------
-Rueda 
-Rueda 
-Chasis
-Motor
-Pintura roja
---------------------
- */
