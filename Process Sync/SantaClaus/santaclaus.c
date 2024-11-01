b49af8137a1cd7034e8b689069109514843b8682
nicolastradivarius <gonzaleznicolas114@outlook.com>
2024-11-01 01:58:55 -0300

    Correccion Santa Claus - Ahora se utilizan las operaciones y semántica de semáforos en lugar de variables con los valores de los semáforos - Funcionando "correctamente" (igual que antes) - El único problema es la cantidad de iteraciones, cuando Santa completa las 100 (o cualquiera sea la macro REPETITIONS), el proceso se va a bloquear. El hecho que Santa complete sus 100 iteraciones es una buena señal, pero estaría bueno ver si se puede lograr que todos los hilos finalicen su ejecución normalmente.

--- Process Sync/SantaClaus/santaclaus.c
+++ Process Sync/SantaClaus/santaclaus.c
@@ -11,93 +11,76 @@
 
 #define REPETITIONS 100
 
-/**
- * Santa Claus duerme en su tienda en el Polo Norte y sólo puede ser despertado por:
- *  - los nueve renos, una vez que regresan de sus vacaciones en el Pacífico Sur,
- *  - algunos de los elfos que están teniendo dificultades para hacer juguetes.
- * Para permitir que Santa descanse lo mejor posible, los elfos solo pueden despertarlo cuando tres de ellos
- * tienen problemas.
- * Cuando estos tres elfos están resolviendo sus problemas, cualquier otro elfo que quiera visitar a Santa debe
- * esperar a que estos elfos terminen.
- * En el momento que el último reno arriba al Polo Norte, este debe ir a buscar a Santa mientras los demás esperan
- * en una cálida cabaña antes de ser enganchados al trineo.
- * Si Santa se despierta y encuentra a tres elfos esperando en la puerta de su tienda, junto con el último reno
- * que ha regresado de los trópicos, atiende primero a los renos, ya que es más importante preparar su trineo.
- *
- * (a) Resolver este problema utilizando hilos y semáforos para su sincronización.
- * (b) Explique brevemente el modelo implementado.
- */
-
-sem_t sem_renos, sem_elfos, sem_santa, sem_santaVolvio;
+sem_t sem_santa, sem_santaVolvio;
 sem_t sem_puerta_elfos; // Para que los elfos esperen a que Santa termine de ayudar a los elfos (simula una puerta). Es binario
+sem_t sem_renos;
+sem_t sem_elfos;
 pthread_mutex_t mutAyuda;
 pthread_t threads[3];
 
-void *thread_santa()
-{
-    int cant_renos;
-    int cant_elfos;
+void *thread_santa() {
 
-    for (int i = 0; i < REPETITIONS; i++)
-    {
+    for (int i = 0; i < REPETITIONS; i++) {
+        printf("Iteración Santa %i\n", i + 1);
         sem_wait(&sem_santa);
         printf("\nSanta Claus: Despierta\n");
-        sleep(2);
+        // sleep(2);
 
-        sem_getvalue(&sem_renos, &cant_renos);
-
-        if (cant_renos == 9)
-        {
+        // si están los 9 renos esperando...
+        if (sem_trywait((&sem_renos)) == -1) {
+            
             pthread_mutex_lock(&mutAyuda);
 
             printf("\nSanta Claus: Preparando trineo");
             fflush(stdout);
-            sleep(1);
-            for (int i = 0; i < 9; i++)
-            {
-                sem_wait(&sem_renos);
-            }
+            // sleep(1);
 
             printf("\nSanta Claus: Trineo listo");
-            fflush(stdout);
             printf("\nSanta Claus: Entregando regalos\n\nSanta volverá en 5 segundos\n");
-            fflush(stdout);
-            for (int i = 5; i > 0; i--)
-            {
+            
+            for (int i = 5; i > 0; i--) {
                 printf(". ");
                 fflush(stdout);
-                sleep(1);
+                // sleep(1);
             }
             
             printf("\nSanta Claus: Volvió\n\n");
-            fflush(stdout);
+
+            // si aparecieron elfos mientras estaba viajando, hicieron un signal de más
+            // debo consumirlo, total los voy a atender cuando vuelva
+            if (sem_trywait(&sem_elfos) == -1) {
+                sem_wait(&sem_santa);
+            } else {
+                sem_post(&sem_elfos);
+            }
+
+            for (int i = 0; i < 9; i++) {
+                sem_post(&sem_renos);
+            }
 
             sem_post(&sem_santaVolvio);
 
             pthread_mutex_unlock(&mutAyuda);
+        } else {
+            sem_post(&sem_renos);
         }
 
-        sem_getvalue(&sem_elfos, &cant_elfos);
-
-        if (cant_elfos >= 3 && cant_renos == 9)
-        {
-            sem_wait(&sem_santa); // consumir el signal que se hizo de más porque llegaron elfos en mitad del viaje
-        }
-
-        if (cant_elfos >= 3)
-        {
+        // si hay 3 elfos esperando...
+        if (sem_trywait((&sem_elfos)) == -1) {
+            
             pthread_mutex_lock(&mutAyuda);
             printf("\nSanta Claus: Ayudando a los elfos\n");
-            sleep(1);
+            // sleep(1);
 
-            for (int i = 0; i < 3; i++)
-            {
-                sem_wait(&sem_elfos);
+            for (int i = 0; i < 3; i++) {
+                sem_post(&sem_elfos);
                 printf("\nSanta Claus: Ayudando al %i° elfo \n", i + 1);
-                sleep(1);
+                // sleep(1);
             }
             sem_post(&sem_puerta_elfos);
             pthread_mutex_unlock(&mutAyuda);
+        } else {
+            sem_post(&sem_elfos);
         }
 
         printf("\nSanta Claus: Volviendo a dormir\n\n");
@@ -107,19 +90,17 @@ void *thread_santa()
     return NULL;
 }
 
-void *thread_reno()
-{
-    for (int i = 0; i < REPETITIONS; i++)
-    {
+void *thread_reno() {
+    for (int i = 0; i < REPETITIONS; i++) {
+        printf("Iteración Renos %i\n", i + 1);
         sem_wait(&sem_santaVolvio);
-        sleep(5);
+        // sleep(5);
 
-        for (int i = 0; i < 9; i++)
-        {
-            sem_post(&sem_renos);
+        for (int i = 0; i < 9; i++) {
+            sem_wait(&sem_renos);
             printf("\nLlega reno %i\n", i + 1);
             fflush(stdout);
-            sleep(2);
+            // sleep(2);
         }
 
         sem_post(&sem_santa);
@@ -128,19 +109,16 @@ void *thread_reno()
     return NULL;
 }
 
-void *thread_elfo()
-{
-    for (int i = 0; i < REPETITIONS; i++)
-    {
+void *thread_elfo() {
+    for (int i = 0; i < REPETITIONS; i++) {
+        printf("Iteración Elfos %i\n", i + 1);
         sem_wait(&sem_puerta_elfos);
 
-        for (int j = 0; j < 3; j++)
-        {
-            sleep(3);
-            sem_post(&sem_elfos);
+        for (int j = 0; j < 3; j++) {
+            sem_wait(&sem_elfos);
 
             printf("\nElfo %i tiene problema\n", j + 1);
-            fflush(stdout);
+            // sleep(3);
         }
 
         printf("\nYa somos 3 elfos, vamos a acudir a Santa\n");
@@ -151,14 +129,13 @@ void *thread_elfo()
     return NULL;
 }
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
     system("clear");
 
-    sem_init(&sem_renos, 0, 0);
-    sem_init(&sem_elfos, 0, 0);
     sem_init(&sem_santa, 0, 0);
     sem_init(&sem_santaVolvio, 0, 1);
+    sem_init(&sem_renos, 0, 9);
+    sem_init(&sem_elfos, 0, 3);
     sem_init(&sem_puerta_elfos, 0, 1);
     pthread_mutex_init(&mutAyuda, NULL);
 
@@ -166,8 +143,7 @@ int main(int argc, char const *argv[])
     pthread_create(&threads[1], NULL, thread_elfo, NULL);
     pthread_create(&threads[2], NULL, thread_santa, NULL);
 
-    for (int i = 0; i < 3; i++)
-    {
+    for (int i = 0; i < 3; i++) {
         pthread_join(threads[i], NULL);
     }
 
@@ -175,6 +151,7 @@ int main(int argc, char const *argv[])
     sem_destroy(&sem_elfos);
     sem_destroy(&sem_santa);
     sem_destroy(&sem_santaVolvio);
+    sem_destroy(&sem_puerta_elfos);
     pthread_mutex_destroy(&mutAyuda);
 
     return 0;
