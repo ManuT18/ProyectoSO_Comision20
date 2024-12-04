72cd03d6d45f53ce9d5145702e4354860ad0b571
nicolastradivarius <gonzaleznicolas114@outlook.com>
2024-12-03 23:21:15 -0300

    Actualización SantaClaus - Mejoras en el código de las entidades. - Se redujo la cantidad de semáforos empleados en el ejercicio. - Se agregó una constante TIMES_RENOS_COMEBACK que sirve para simular que los renos se van y vuelven de vacaciones. Se utiliza para crear nuevos hilos de renos cuando terminan los anteriores. - En `actividad_elfos` ya no se utiliza el parámetro. Si se encuentra una manera de identificar qué elfo tiene problemas, agregarlo.

--- src/Process_Sync/SantaClaus/santaclaus.c
+++ src/Process_Sync/SantaClaus/santaclaus.c
@@ -9,18 +9,16 @@
 #include <semaphore.h>
 #include <pthread.h>
 
-#define REPETITIONS 60
+#define REPETITIONS 10
+#define TIMES_RENOS_COMEBACK 5 // cuántas veces vacacionan los renos
 #define CANT_RENOS 9
-#define CANT_ELFOS 13
+#define CANT_ELFOS 12
 
 sem_t sem_santa; // para que Santa se despierte. Tiene un comportamiento preferiblemente binario (ver la función actividad_santa para entender por qué no es exactamente binario).
-sem_t sem_santa_volvio; // para indicar que Santa volvió de entregar los regalos. Es binario
-sem_t sem_puerta_elfos; // para que los elfos nuevos esperen atrás de una puerta a que Santa termine de ayudar a los elfos viejos (simula una puerta que se cierra cuando hay 3 elfos adentro). Es binario
-sem_t sem_maxima_renos; // indica la cantidad máxima de renos 
-sem_t sem_elfos_ayuda; // indica la cantidad de elfos máxima que pueden pedir ayuda a Santa
-sem_t sem_elfos_grupo; // indica la cantidad corriente de elfos
-sem_t sem_renos; // indica la cantidad corriente de renos
-sem_t sem_renos_pueden_entrar; // para indicar cuándo pueden volver los nueve renos
+sem_t sem_renos_grupo; // indica la cantidad máxima de renos y sirve para sincronizar los grupos de nueve renos con los que Santa va a atender, así como para identificar al último reno encargado de despertarlo
+sem_t sem_max_renos; // indica la cantidad máxima de renos y sirve para que los renos que se Santa libera no acudan a él inmediatamente después
+sem_t sem_elfos_grupo; // indica la cantidad máxima de elfos y sirve para sincronizar los grupos de tres elfos con los que Santa va a atender, así como para identificar al último elfo del grupo encargado de despertar a Santa.
+sem_t sem_max_elfos; // indica la cantidad máxima de elfos y sirve para que otros elfos no quieran acudir a Santa cuando ya haya un grupo con él
 
 pthread_t threads_renos[CANT_RENOS];
 pthread_t threads_elfos[CANT_ELFOS];
@@ -34,7 +32,7 @@ void *actividad_santa() {
         printf("\nSanta Claus: Despierta\n");
 
         // si están los 9 renos esperando...
-        if (sem_trywait(&sem_maxima_renos) == -1) {
+        if (sem_trywait(&sem_renos_grupo) == -1) {
             printf("Santa Claus: Preparando trineo\n");
 
             printf("Santa Claus: Trineo listo\n");
@@ -49,38 +47,41 @@ void *actividad_santa() {
             printf("\n\nSanta Claus: Volvió\n\n");
 
             // si aparecieron elfos mientras estaba viajando, hicieron un signal de más en sem_santa. Debo consumirlo, total los voy a atender cuando vuelva
-            if (sem_trywait(&sem_elfos_ayuda) == -1) {
+            if (sem_trywait(&sem_elfos_grupo) == -1) {
                 sem_wait(&sem_santa);
             } else {
-                sem_post(&sem_elfos_ayuda);
+                sem_post(&sem_elfos_grupo);
             }
 
             // libero a los nueve renos
-            for (int i = 0; i < 9; i++) {
-                sem_wait(&sem_renos);
-                sem_post(&sem_maxima_renos);
+            for (int i = 0; i < CANT_RENOS; i++) {
+                sem_post(&sem_renos_grupo);
             }
-            sem_post(&sem_renos_pueden_entrar);
 
-            sem_post(&sem_santa_volvio);
+            for (int i = 0; i < CANT_RENOS; i++) {
+                sem_post(&sem_max_renos);
+            }
+
         } else {
-            sem_post(&sem_maxima_renos);
+            sem_post(&sem_renos_grupo);
         }
 
         // si hay 3 elfos esperando...
-        if (sem_trywait((&sem_elfos_ayuda)) == -1) {
+        if (sem_trywait((&sem_elfos_grupo)) == -1) {
             printf("\nSanta Claus: Ayudando a los elfos\n");
 
             for (int i = 0; i < 3; i++) {
-                sem_post(&sem_elfos_ayuda);
                 printf("\nSanta Claus: Ayudando al %i° elfo \n", i + 1);
+                sem_post(&sem_elfos_grupo);
             }
 
             printf("\nSanta Claus: Terminé de ayudar a los elfos\n");
 
-            sem_post(&sem_puerta_elfos);
+            for (int i = 0; i < 3; i++) {
+                sem_post(&sem_max_elfos);
+            }
         } else {
-            sem_post(&sem_elfos_ayuda);
+            sem_post(&sem_elfos_grupo);
         }
 
         printf("\nSanta Claus: Volviendo a dormir\n\n");
@@ -104,26 +105,18 @@ void *actividad_reno(void *args) {
     int id_reno = (int)(size_t)args;
     id_reno++;
 
-    pthread_mutex_lock(&mutex_renos);
-
-    // el primer reno es el que le da el paso a los demás
-    // hasta que santa no diga que los renos pueden entrar, no entra ninguno
-    if (sem_trywait(&sem_renos) == -1) {
-        sem_wait(&sem_renos_pueden_entrar);
-    } else {
-        sem_post(&sem_renos);
-    }
-    sem_post(&sem_renos);
-    
+    sem_wait(&sem_max_renos);
     printf("\nReno %i volvió de vacaciones\n", (int)id_reno);
 
-    sem_wait(&sem_maxima_renos);
-    // si soy el último reno en llegar, despierto a Santa
-    if (sem_trywait(&sem_maxima_renos) == -1) {
+    pthread_mutex_lock(&mutex_renos);
+
+    sem_wait(&sem_renos_grupo);
+
+    if (sem_trywait(&sem_renos_grupo) == -1) {
         printf("\nYa somos 9 renos, vamos a acudir a Santa\n");
         sem_post(&sem_santa);
     } else {
-        sem_post(&sem_maxima_renos);
+        sem_post(&sem_renos_grupo);
     }
 
     pthread_mutex_unlock(&mutex_renos);
@@ -132,27 +125,18 @@ void *actividad_reno(void *args) {
 }
 
 void *actividad_elfo(void *args) {
-    int id_elfo = (int)(size_t)args;
-    id_elfo++;
+    sem_wait(&sem_max_elfos);
 
     pthread_mutex_lock(&mutex_elfos);
 
-    // si ya hay elfos pidiendo ayuda a santa, debo esperar a que terminen
-    if (sem_trywait(&sem_puerta_elfos) == -1) {
-        
-    } else {
-        sem_post(&sem_puerta_elfos);
-    }
+    sem_wait(&sem_elfos_grupo);
 
-    sem_wait(&sem_elfos_ayuda);
-    printf("\nElfo %i necesita ayuda\n", (int)id_elfo);
     // si soy el último elfo de un grupo de tres, acudimos a Santa
-    if (sem_trywait(&sem_elfos_ayuda) == -1) {
+    if (sem_trywait(&sem_elfos_grupo) == -1) {
         printf("\nYa somos 3 elfos, vamos a acudir a Santa\n");
-        sem_wait(&sem_puerta_elfos);
         sem_post(&sem_santa);
     } else {
-        sem_post(&sem_elfos_ayuda);
+        sem_post(&sem_elfos_grupo);
     }
 
     pthread_mutex_unlock(&mutex_elfos);
@@ -162,13 +146,10 @@ void *actividad_elfo(void *args) {
 
 int main(int argc, char const *argv[]) {
     sem_init(&sem_santa, 0, 0);
-    sem_init(&sem_santa_volvio, 0, 1);
-    sem_init(&sem_maxima_renos, 0, 9);
-    sem_init(&sem_renos, 0, 0);
-    sem_init(&sem_elfos_ayuda, 0, 3);
-    sem_init(&sem_puerta_elfos, 0, 1);
-    sem_init(&sem_renos_pueden_entrar, 0, 1);
-    sem_init(&sem_elfos_grupo, 0, 0);
+    sem_init(&sem_max_renos, 0, 9);
+    sem_init(&sem_elfos_grupo, 0, 3);
+    sem_init(&sem_max_elfos, 0, 3);
+    sem_init(&sem_renos_grupo, 0, 9);
 
     pthread_mutex_init(&mutex_renos, NULL);
     pthread_mutex_init(&mutex_elfos, NULL);
@@ -176,27 +157,36 @@ int main(int argc, char const *argv[]) {
     for (int i = 0; i < CANT_RENOS; i++) {
         pthread_create(&threads_renos[i], NULL, actividad_reno, (void *)(size_t)i);
     }
+
     for (int i = 0; i < CANT_ELFOS; i++) {
         pthread_create(&threads_elfos[i], NULL, actividad_elfo, (void *)(size_t)i);
     }
+
     pthread_create(&thread_santa, NULL, actividad_santa, NULL);
+
+    for (int i = 0; i < TIMES_RENOS_COMEBACK; i++) {
+        for (int j = 0; j < CANT_RENOS; j++) {
+            pthread_join(threads_renos[j], NULL);
+            pthread_create(&threads_renos[j], NULL, actividad_reno, (void *)(size_t)j);
+        }
+    }
 
     for (int i = 0; i < CANT_RENOS; i++) {
         pthread_join(threads_renos[i], NULL);
     }
+
     for (int i = 0; i < CANT_ELFOS; i++) {
         pthread_join(threads_elfos[i], NULL);
     }
+
     pthread_join(thread_santa, NULL);
 
-    sem_destroy(&sem_maxima_renos);
-    sem_destroy(&sem_elfos_ayuda);
-    sem_destroy(&sem_santa);
-    sem_destroy(&sem_santa_volvio);
-    sem_destroy(&sem_puerta_elfos);
-    sem_destroy(&sem_renos);
-    sem_destroy(&sem_renos_pueden_entrar);
+    sem_destroy(&sem_max_renos);
     sem_destroy(&sem_elfos_grupo);
+    sem_destroy(&sem_santa);
+    sem_destroy(&sem_elfos_grupo);
+    sem_destroy(&sem_max_elfos);
+    sem_destroy(&sem_renos_grupo);
 
     pthread_mutex_destroy(&mutex_renos);
     pthread_mutex_destroy(&mutex_elfos);
