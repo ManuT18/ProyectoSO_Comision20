72ff2281ffcd24870390885ca2acb8559cb77c85
nicolastradivarius <gonzaleznicolas114@outlook.com>
2024-12-04 15:23:27 -0300

    Eliminación de semáforo innecesario

--- src/Process_Sync/SantaClaus/santaclaus.c
+++ src/Process_Sync/SantaClaus/santaclaus.c
@@ -9,13 +9,12 @@
 #include <semaphore.h>
 #include <pthread.h>
 
-#define TIMES_RENOS_COMEBACK 3 // cuántas veces vacacionan los renos
+#define TIMES_RENOS_COMEBACK 7 // cuántas veces vacacionan los renos
 #define CANT_RENOS 9
-#define CANT_ELFOS 28 // si no es multiplo de 3, van a quedar elfos sin atender
+#define CANT_ELFOS 27 // si no es multiplo de 3, van a quedar elfos sin atender
 
 sem_t sem_santa; // para que Santa se despierte. Tiene un comportamiento preferiblemente binario (ver la función actividad_santa para entender por qué no es exactamente binario).
 sem_t sem_renos_grupo; // indica la cantidad máxima de renos y sirve para sincronizar los grupos de nueve renos con los que Santa va a atender, así como para identificar al último reno encargado de despertarlo
-sem_t sem_max_renos; // indica la cantidad máxima de renos y sirve para que los renos que se Santa libera no acudan a él inmediatamente después
 sem_t sem_elfos_grupo; // indica la cantidad máxima de elfos y sirve para sincronizar los grupos de tres elfos con los que Santa va a atender, así como para identificar al último elfo del grupo encargado de despertar a Santa.
 sem_t sem_max_elfos; // indica la cantidad máxima de elfos y sirve para que otros elfos no quieran acudir a Santa cuando ya haya un grupo con él
 
@@ -38,7 +37,7 @@ void *actividad_santa() {
             printf("Santa Claus: Entregando regalos\n");
             printf("Santa Claus: Volverá en un momento\n");
             for (int i = 5; i > 0; i--) {
-                sleep(1);
+                // sleep(1);
                 printf(". ");
                 fflush(stdout);
             }
@@ -58,7 +57,6 @@ void *actividad_santa() {
             }
 
             for (int i = 0; i < CANT_RENOS; i++) {
-                sem_post(&sem_max_renos);
             }
 
         } else {
@@ -73,7 +71,7 @@ void *actividad_santa() {
 
             for (int i = 0; i < 3; i++) {
                 printf("\nSanta Claus: Ayudando al %i° elfo \n", i + 1);
-                sleep(1);
+                // sleep(1);
                 sem_post(&sem_elfos_grupo);
             }
 
@@ -107,13 +105,12 @@ void *actividad_reno(void *args) {
     int id_reno = (int)(size_t)args;
     id_reno++;
 
-    sem_wait(&sem_max_renos);
 
     pthread_mutex_lock(&mutex_renos);
 
-    printf("\nReno %i volvió de vacaciones\n", (int)id_reno);
-    sleep(1);
     sem_wait(&sem_renos_grupo);
+    printf("\nReno %i volvió de vacaciones\n", (int)id_reno);
+    // sleep(1);
 
     // si soy el último reno de un grupo de nueve, acudimos a Santa
     if (sem_trywait(&sem_renos_grupo) == -1) {
@@ -136,7 +133,7 @@ void *actividad_elfo(void *args) {
 
     pthread_mutex_lock(&mutex_elfos);
     printf("\nElfo %i necesita ayuda\n", (int)id_elfo);
-    sleep(1);
+    // sleep(1);
 
     sem_wait(&sem_elfos_grupo);
     // si soy el último elfo de un grupo de tres, acudimos a Santa
@@ -154,7 +151,6 @@ void *actividad_elfo(void *args) {
 
 int main(int argc, char const *argv[]) {
     sem_init(&sem_santa, 0, 0);
-    sem_init(&sem_max_renos, 0, 9);
     sem_init(&sem_elfos_grupo, 0, 3);
     sem_init(&sem_max_elfos, 0, 3);
     sem_init(&sem_renos_grupo, 0, 9);
@@ -175,6 +171,8 @@ int main(int argc, char const *argv[]) {
     for (int i = 0; i < TIMES_RENOS_COMEBACK; i++) {
         for (int j = 0; j < CANT_RENOS; j++) {
             pthread_join(threads_renos[j], NULL);
+        }
+        for (int j = 0; j < CANT_RENOS; j++) {
             pthread_create(&threads_renos[j], NULL, actividad_reno, (void *)(size_t)j);
         }
     }
@@ -189,10 +187,8 @@ int main(int argc, char const *argv[]) {
 
     pthread_join(thread_santa, NULL);
 
-    sem_destroy(&sem_max_renos);
     sem_destroy(&sem_elfos_grupo);
     sem_destroy(&sem_santa);
-    sem_destroy(&sem_elfos_grupo);
     sem_destroy(&sem_max_elfos);
     sem_destroy(&sem_renos_grupo);
 
